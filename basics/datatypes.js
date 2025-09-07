13cde81150193ec32e591e0dbd0af62b15b0834e
pooja2188 <poojarani87223@gmail.com>
2025-09-07 16:03:15 +0000

    datatype summary

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -28,12 +28,44 @@ undefined->Nan
 ""->false
 */
 
-let str1="Pooja"
-let str2=" Kudal"
-let str3=str1+str2
-console.log(str3)
-console.log("1"+"2")
-console.log("1"+2)
-console.log(1+"2")
-console.log("1"+2+2)
-console.log(1+2+"2")
+// let str1="Pooja"
+// let str2=" Kudal"
+// let str3=str1+str2
+// console.log(str3)
+// console.log("1"+"2")
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+//Primitive data types
+//7 types- string,Number,Boolean,null,undefined,Symbol ,BigInt
+
+//Reference (Non Primitive)
+//Array,Objects,Functions
+
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id==anotherId)
+
+//arrays
+const heroes=["Shaktiman","Naagraj","Doga"]
+
+//object
+let myObj={
+    name:"Pooja",
+    age:20
+}
+console.log(myObj)
+console.log(heroes)
+
+//functions in javascript
+const myFun=function(){
+    console.log("Hello World!");
+}
+myFun()
+
+console.log(typeof(heroes))
+console.log(typeof(myObj))
+console.log(typeof(myFun))
