716aa4ddf9a3f4efac3e27ee7053ed46fed2e71d
pooja2188 <poojarani87223@gmail.com>
2025-09-07 18:20:00 +0000

    number and math library

---
+++ basics/number.js
@@ -0,0 +1,28 @@
+// const score=400
+// console.log(score)
+// const balance=new Number(100)
+// console.log(balance)
+// console.log(typeof(balance))
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// const otherNumber=1223.8996
+// console.log(otherNumber.toPrecision(3))
+
+
+
+// console.log(Math.abs(-45))
+// console.log(Math.round(4.7))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.7))
+// console.log(Math.ceil(-4.3))
+// console.log(Math.floor(-4.7))
+
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10));
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
